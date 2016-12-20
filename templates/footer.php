d29e0e25a29fb35e0666a247ebfb976d7e0b8e43
littlesumo <bvillanueva@Bryans-Mac-Pro-2.local>
2016-12-20 13:49:07 -0800

    Theme dev

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,24 @@
 <footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4">
+			<?//php dynamic_sidebar('sidebar-footer'); ?>	
+			<span class="copyright">&copy; 2003-<?php echo date('Y'); ?> The Rand Group, LLC</span>			
+			</div>
+			<div class="col-lg-8 foot-links">
+		<?php
+	      if (has_nav_menu('footer_navigation')) :
+	        wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav']);
+	      endif;
+	    ?>					
+			</div>
+		</div>
+		
+	</div>
 </footer>
+
+
+
+
+
+
